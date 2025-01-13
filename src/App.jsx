c0e14941f12a82b875e664d787c82484632e0705
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-13 17:00:11 +0100

    routers, homepage and aboutpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 // import { useState } from 'react'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import HomePage from "../pages/Homepage";
+import Aboutpage from "../pages/AboutPage";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Header />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout/>} path="/">
+        <Route index element={<HomePage/>}/>
+        <Route path="/about" element={<Aboutpage/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
