437a62f04867211602313ecaf8616f0746aca723
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-14 20:39:33 +0100

    carousel added

--- src/pages/movies/IndexMoviesPage.jsx
+++ src/pages/movies/IndexMoviesPage.jsx
@@ -10,6 +10,8 @@ export default function IndexMoviesPage() {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
+        // console.log(data.movies);
+
         setMovies(data.movies);
       });
   }, []);
@@ -17,6 +19,42 @@ export default function IndexMoviesPage() {
   return (
     <>
       <div className="container">
+        <div
+          id="carouselExampleRide"
+          className="carousel slide"
+          data-bs-ride="true"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="https://i.pinimg.com/1200x/0b/36/63/0b36632ceffe66aaa1e704347a6311be.jpg" className="d-block w-100" alt="..." />
+              
+            </div>
+            <div className="carousel-item">
+              <img src="https://th-thumbnailer.cdn-si-edu.com/PQXRUGQnbG8E6eJ5lE2OwwNi6hk=/fit-in/1200x0/https%3A%2F%2Ftf-cmsv2-smithsonianmag-media.s3.amazonaws.com%2Ffiler%2FThe-Godfather-Effect-Don-Vito-631.jpg" className="d-block w-100 h-500" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://tgtrs.wordpress.com/wp-content/uploads/2021/09/unnamed-1.jpg" className="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleRide"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleRide"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
         <div className="row m-5">
           {movies.map((movie) => (
             <div key={movie.id} className="col-4 d-flex  mt-5">
@@ -28,7 +66,7 @@ export default function IndexMoviesPage() {
                 />
                 <div className="card-body">
                   <h5 className="card-title">{movie.title}</h5>
-                  
+
                   <Link to={"/movies/" + movie.id} className="btn btn-primary">
                     {movie.title}
                   </Link>
