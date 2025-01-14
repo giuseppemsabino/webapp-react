cb864b2da067e9afcb4185e3c5fea33a3edac16b
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-14 17:07:39 +0100

    cards added

--- src/pages/movies/IndexMoviesPage.jsx
+++ src/pages/movies/IndexMoviesPage.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
 export default function IndexMoviesPage() {
   const [movies, setMovies] = useState([]);
 
@@ -21,9 +20,21 @@ export default function IndexMoviesPage() {
         <h1>Movie List</h1>
 
         {movies.map((movie) => (
-          <li key={movie.id}>
-            <Link to={'/movies/' + movie.id}>{movie.title}</Link>
-          </li>
+          // <button to={'/movies/' + movie.id}>{movie.title}</button>
+
+          <div key={movie.id} className="card col-4 d-flex" >
+            <img src={movie.image} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{movie.title}</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <Link to={"/movies/" + movie.id} className="btn btn-primary">
+                {movie.title}
+              </Link>
+            </div>
+          </div>
         ))}
       </div>
     </>
